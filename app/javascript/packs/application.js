df04fa63158c1e338926784d6c47d27aa860365b
Laguna1 <lagunazoomarket@gmail.com>
2021-04-09 19:39:56 +0300

    Add bootstrap styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap";
+import "../stylesheets/application" 
+ 
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
 
 Rails.start()
 Turbolinks.start()
