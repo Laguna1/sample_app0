66fa398a5342937e2e968cdb6e1f25478e1b8928
Laguna1 <lagunazoomarket@gmail.com>
2021-04-10 01:16:57 +0300

    Add tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contacts_path
+    assert_select 'a[href=?]', signup_path
   end
 end
